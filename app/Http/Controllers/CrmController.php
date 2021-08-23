6cc160acad87ba2e60c24326fa2e63011bcf1de5
PilotJinix <agungsantoso557@gmail.com>
2021-08-23 21:16:32 +0700

    UP

---
+++ app/Http/Controllers/CrmController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CrmController extends Controller
+{
+    public function index(){
+        $data = DB::table("authors")->select([
+            DB::raw("count(*) as jumlah"),
+            DB::raw("DATE(created_at) as tanggal")
+        ])
+            ->groupBy("tanggal")
+            ->get();
+        dd($data);
+        return view("welcome");
+    }
+}
